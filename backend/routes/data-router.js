c87635ee5821e2398e269c212f2b55c4b8ae9c32
karvaton <karvaton220@gmail.com>
2021-12-19 12:11:13 +0200

    декомпозиція бекенда

--- backend/routes/data-router.js
+++
@@ -1,9 +0,0 @@
-import Router from "express";
-import * as dataControl from "../controller/data-controller.js";
-
-const router = new Router();
-
-router.get('/parameters/:login', dataControl.getParameters);
-router.post('/parameters', dataControl.getParameters);
-
-export default router;
